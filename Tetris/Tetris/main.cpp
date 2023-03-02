e42fdd88e8541559b78f9de670343243f1e19028
leopoldrocks123 <leopoldrocks535@gmail.com>
2023-03-01 23:35:11 -0600

    first commit

---
+++ Tetris/Tetris/main.cpp
@@ -0,0 +1,57 @@
+//
+//  main.cpp
+//  Tetris
+//
+//  Created by Leo Tejkowski on 3/1/23.
+//
+
+#include <iostream>
+using namespace std;
+
+wstring tetromino[7];
+
+int main(){
+    
+    // Create assets
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    
+    tetromino[1].append(L"..X.");
+    tetromino[1].append(L".XX.");
+    tetromino[1].append(L".X..");
+    tetromino[1].append(L"....");
+    
+    tetromino[2].append(L".X...");
+    tetromino[2].append(L".XX.");
+    tetromino[2].append(L"..X.");
+    tetromino[2].append(L"....");
+    
+    tetromino[3].append(L"....");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L"....");
+    
+    tetromino[4].append(L"..X.");
+    tetromino[4].append(L"..X.");
+    tetromino[4].append(L"..X.");
+    tetromino[4].append(L"..X.");
+    
+    tetromino[5].append(L"....");
+    tetromino[5].append(L".XX.");
+    tetromino[5].append(L"..X.");
+    tetromino[5].append(L"..X.");
+    
+    tetromino[6].append(L"....");
+    tetromino[6].append(L".XX.");
+    tetromino[6].append(L".X..");
+    tetromino[6].append(L".X..");
+    
+    
+    
+    
+    
+    
+    return 0;
+}
