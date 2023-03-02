31e44def0df119c24bbe87181e4651efe162ef82
leopoldrocks123 <leopoldrocks535@gmail.com>
2023-03-01 23:50:27 -0600

    Made the border

--- Tetris/Tetris/main.cpp
+++ Tetris/Tetris/main.cpp
@@ -9,6 +9,20 @@
 using namespace std;
 
 wstring tetromino[7];
+int nFieldWidth = 12;
+int nFieldHeight = 18;
+unsigned char *pField = nullptr;
+
+int Rotate(int px, int py, int r){
+    switch(r % 4){
+        case 0: return py * 4 + px;         // 0 degrees    r = 0
+        case 1: return 12 + py - (px*4);    // 90 degrees   r = 1
+        case 2: return 15 - (py*4) - px;    // 180 degrees  r = 2
+        case 3: return 3 - py + (px*4);     // 270 degrees  r = 3
+    }
+   
+    return 0;
+}
 
 int main(){
     
@@ -48,7 +62,10 @@ int main(){
     tetromino[6].append(L".X..");
     tetromino[6].append(L".X..");
     
-    
+    pField = new unsigned char[nFieldWidth*nFieldHeight];
+    for (int x = 0; x < nFieldWidth; x++)       // Board boundary
+        for (int y = 0; y < nFieldHeight; y++)
+            pField[y*nFieldWidth + x] = (x == 0 | x == nFieldWidth - 1 || y == nFieldHeight - 1) ? 9:0;
     
     
     
